a04ee92420b60aa3ec83666e549fa4e1715d4589
Netanel Revah <netanelrevah@outlook.com>
2025-04-25 17:52:12 +0300

    multi and watch kind of working, need big refactor :(

--- pyvalkey/commands/hyperloglog_commands.py
+++ pyvalkey/commands/hyperloglog_commands.py
@@ -4,7 +4,7 @@ from pyvalkey.commands.router import command
 from pyvalkey.resp import ValueType
 
 
-@command(b"pfadd", [b"stream", b"write", b"fast"])
+@command(b"pfadd", {b"stream", b"write", b"fast"})
 class HyperLogLogAdd(Command):
     key: bytes = positional_parameter()
     elements: list[bytes] = positional_parameter()
@@ -13,7 +13,7 @@ class HyperLogLogAdd(Command):
         return 1
 
 
-@command(b"pfcount", [b"stream", b"write", b"fast"])
+@command(b"pfcount", {b"stream", b"write", b"fast"})
 class HyperLogLogCount(Command):
     keys: list[bytes] = positional_parameter()
 
@@ -21,7 +21,7 @@ class HyperLogLogCount(Command):
         return 1
 
 
-@command(b"pfdebug", [b"stream", b"write", b"fast"])
+@command(b"pfdebug", {b"stream", b"write", b"fast"})
 class HyperLogLogDebug(Command):
     subcommand: bytes = positional_parameter()
     key: bytes = positional_parameter()
@@ -30,7 +30,7 @@ class HyperLogLogDebug(Command):
         return 1
 
 
-@command(b"pfmerge", [b"stream", b"write", b"fast"])
+@command(b"pfmerge", {b"stream", b"write", b"fast"})
 class HyperLogLogMerge(Command):
     destination_key: bytes = positional_parameter()
     source_keys: list[bytes] = positional_parameter()
@@ -39,7 +39,7 @@ class HyperLogLogMerge(Command):
         return 1
 
 
-@command(b"pfselftest", [b"stream", b"write", b"fast"])
+@command(b"pfselftest", {b"stream", b"write", b"fast"})
 class HyperLogLogSelfTest(Command):
     def execute(self) -> ValueType:
         return 1
