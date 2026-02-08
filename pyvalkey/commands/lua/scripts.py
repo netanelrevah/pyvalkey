d2c08992c9856658f04f555db5bc2a78f74c8ef9
Netanel Revah <netanelrevah@outlook.com>
2026-02-08 10:55:33 +0200

    refactor mortal_lua_function to set and clear global KEYS and ARGV for improved context handling

--- pyvalkey/commands/lua/scripts.py
+++ pyvalkey/commands/lua/scripts.py
@@ -1,7 +1,10 @@
 LUA_RUN_WITH_TIMEOUT = b"""
-function mortal_lua_function(f, timeout_seconds, should_stop, KEYS, ARGV)
+function(f, timeout_seconds, should_stop, KEYS, ARGV)
     local start_time = os.time()
     
+    _G.KEYS = KEYS
+    _G.ARGV = ARGV
+
     debug.sethook(function()
         if os.difftime(os.time(), start_time) > timeout_seconds or should_stop() then
             error("Timeout reached!")
@@ -11,6 +14,9 @@ function mortal_lua_function(f, timeout_seconds, should_stop, KEYS, ARGV)
     local status, result = pcall(f)
 
     debug.sethook()
+    
+    _G.KEYS = nil
+    _G.ARGV = nil
 
     return status, result
 end
